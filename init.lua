9e2bae00881bc103b7aa8ce43aea4ff81e3d8ac2
CasimirKaPazi <jul.lutz@gmx.net>
2022-01-07 16:33:07 +0100

    Slowly approximate new spawn to center of players

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
-local RADIUS = 128
-local SPAWN = {x=0, y=0, z=0}
-local timer = 0
+local radius = 128
+local spawn = {x=0, y=0, z=0}
+local center = {x=0, y=0, z=0}
+local timer = 295 -- first run shortly after start
 
 --
 -- Determine center of spawnpoint as mean of player positions
@@ -8,19 +9,33 @@ local timer = 0
 
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime
-	if timer < 60 then return end
+	if timer < 300 then return end -- 300 = 5 minutes
 	timer = 0
 
-	local sx = 0 -- mean x value
-	local sz = 0 -- mean z value
+	local cx = 0
+	local cy = 0
+	local cz = 0
+	local n = 0 -- number of players counted
 	local players = minetest.get_connected_players()
+	if #players < 1 then return end
 	for _,player in ipairs(players) do
 		local pos = player:get_pos()
-		sx = sx+pos.x
-		sz = sz+pos.z
+		-- only count players close to the surface
+		if pos.y >= 0 and pos.y < 256 then
+			cx = cx+pos.x
+			cy = cy+pos.y
+			cz = cz+pos.z
+			n = n + 1
+		end
 	end
-	SPAWN = {x=sx/#players, y=0, z=sz/#players}
-    --print("new Spawn at = "..SPAWN.x..", "..SPAWN.z)
+	center = {x=cx/n, y=cy/n, z=cz/n}
+	-- slowly approximate spawnpoint to new center
+	-- changes faster with more players
+	local sx = math.floor((spawn.x * 10 + center.x * n) / (10+n))
+	local sy = math.floor((spawn.y * 10 + center.y * n) / (10+n))
+	local sz = math.floor((spawn.z * 10 + center.z * n) / (10+n))
+	spawn = {x = sx, y = sy, z = sz}
+    print("spawnarea: new spawn at = "..spawn.x..", "..spawn.y..", "..spawn.z)
 end)
 
 --
@@ -28,31 +43,31 @@ end)
 --
 
 local function findspawn(player)
-	for try=100000, 0, -1 do
-		local pos = {x = SPAWN.x, y = SPAWN.y, z = SPAWN.z}
-		pos.x = SPAWN.x + math.random(-RADIUS, RADIUS)
-		pos.z = SPAWN.z + math.random(-RADIUS, RADIUS)
-		if minetest.forceload_block(pos) then
-			-- Find ground level (0...15)
-			local ground_y = nil
-			for y=16, 0, -1 do
-				local nn = minetest.get_node({x=pos.x, y=y, z=pos.z}).name
-				if nn ~= "air" and nn~= "ignore" then
-					ground_y = y
-					break
-				end
+	for try=100, 0, -1 do
+		local pos = {x = spawn.x, y = spawn.y, z = spawn.z}
+		pos.x = spawn.x + math.random(-radius, radius)
+		pos.y = spawn.y
+		pos.z = spawn.z + math.random(-radius, radius)
+		minetest.forceload_block(pos, true)
+		-- Find ground level (0...15)
+		local ground_y = nil
+		for y=128, -128, -1 do
+			local nn = minetest.get_node({x=pos.x, y=pos.y+y, z=pos.z}).name
+			if nn ~= "air" and nn ~= "ignore" then
+				ground_y = pos.y+y
+				break
 			end
-			if ground_y then
-				pos.y = ground_y
-				if minetest.registered_nodes[minetest.get_node(pos).name].walkable == true and
-					minetest.get_node({x=pos.x, y=pos.y+1, z=pos.z}).name == "air" and
-					minetest.get_node({x=pos.x, y=pos.y+2, z=pos.z}).name == "air" then
-					local pos_spawn = {x=pos.x, y=pos.y+1, z=pos.z}
-					return pos_spawn
-				end
-			end
-			minetest.forceload_free_block(pos)
 		end
+		if ground_y then
+			pos.y = ground_y
+			if minetest.registered_nodes[minetest.get_node(pos).name].walkable == true and
+				minetest.get_node({x=pos.x, y=pos.y+1, z=pos.z}).name == "air" and
+				minetest.get_node({x=pos.x, y=pos.y+2, z=pos.z}).name == "air" then
+				local pos_spawn = {x=pos.x, y=pos.y+1, z=pos.z}
+				return pos_spawn
+			end
+		end
+		minetest.forceload_free_block(pos)
 	end
 end
 
@@ -61,7 +76,7 @@ local function spawnarea(player)
 	if pos then
 		player:setpos(pos)
 	else
-		player:setpos(SPAWN)
+		player:setpos(spawn)
 	end
 end
 
